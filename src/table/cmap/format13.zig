5dae0bdb4f1fe37db60780409a4767e7064f5f2b
Hollin Wilkins <hollinrwilkins@gmail.com>
2024-05-30 18:29:36 -0700

    Format13 implementation.

--- src/table/cmap/format13.zig
+++ src/table/cmap/format13.zig
@@ -1,55 +1,70 @@
-// // https://docs.microsoft.com/en-us/typography/opentype/spec/cmap#format-13-many-to-one-range-mappings
+const std = @import("std");
+const root = @import("../../root.zig");
+const Error = root.Error;
+const Reader = root.Reader;
+const GlyphId = root.GlyphId;
+const LazyArray = root.LazyArray;
+const SequentialMapGroup = @import("./format12.zig").SequentialMapGroup;
 
-// use core::convert::TryFrom;
+pub const Subtable13 = struct {
+    const SequentialMapGroupsList = LazyArray(SequentialMapGroup);
 
-// use super::format12::SequentialMapGroup;
-// use crate::parser::{LazyArray32, Stream};
-// use crate::GlyphId;
+    groups: SequentialMapGroupsList,
 
-// /// A [format 13](https://docs.microsoft.com/en-us/typography/opentype/spec/cmap#format-13-segmented-coverage)
-// /// subtable.
-// #[derive(Clone, Copy)]
-// pub struct Subtable13<'a> {
-//     groups: LazyArray32<'a, SequentialMapGroup>,
-// }
+    pub fn create(data: []const u8) Error!Subtable13 {
+        var reader = Reader.create(data);
+        reader.skip(u16); // format
+        reader.skip(u16); // reserved
+        reader.skip(u16); // length
+        reader.skip(u16); // language
+        const count = reader.readInt(u32) orelse return error.InvalidTable;
+        const groups = SequentialMapGroupsList.read(&reader, count) orelse return error.InvalidTable;
 
-// impl<'a> Subtable13<'a> {
-//     /// Parses a subtable from raw data.
-//     pub fn parse(data: &'a [u8]) -> Option<Self> {
-//         let mut s = Stream::new(data);
-//         s.skip::<u16>(); // format
-//         s.skip::<u16>(); // reserved
-//         s.skip::<u32>(); // length
-//         s.skip::<u32>(); // language
-//         let count = s.read::<u32>()?;
-//         let groups = s.read_array32::<super::format12::SequentialMapGroup>(count)?;
-//         Some(Self { groups })
-//     }
+        return Subtable13{
+            .groups = groups,
+        };
+    }
 
-//     /// Returns a glyph index for a code point.
-//     pub fn glyph_index(&self, code_point: u32) -> Option<GlyphId> {
-//         for group in self.groups {
-//             let start_char_code = group.start_char_code;
-//             if code_point >= start_char_code && code_point <= group.end_char_code {
-//                 return u16::try_from(group.start_glyph_id).ok().map(GlyphId);
-//             }
-//         }
+    pub fn getGlyphIndex(self: Subtable13, codepoint32: u32) ?GlyphId {
+        var iterator = self.groups.iterator();
 
-//         None
-//     }
+        while (iterator.next()) |group| {
+            const start_char_code = group.start_char_code;
+            if (codepoint32 >= start_char_code and codepoint32 <= group.end_char_code) {
+                return @intCast(group.start_glyph_id);
+            }
+        }
 
-//     /// Calls `f` for each codepoint defined in this table.
-//     pub fn codepoints(&self, mut f: impl FnMut(u32)) {
-//         for group in self.groups {
-//             for code_point in group.start_char_code..=group.end_char_code {
-//                 f(code_point);
-//             }
-//         }
-//     }
-// }
+        return null;
+    }
 
-// impl core::fmt::Debug for Subtable13<'_> {
-//     fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result {
-//         write!(f, "Subtable13 {{ ... }}")
-//     }
-// }
+    fn order(codepoint: u32, group: *const SequentialMapGroup) std.math.Order {
+        if (group.start_char_code > codepoint) {
+            return .gt;
+        } else if (group.end_char_code < codepoint) {
+            return .lt;
+        }
+
+        return .eq;
+    }
+
+    pub const Iterator = struct {
+        table: Subtable13,
+        index: usize,
+        i: usize,
+
+        pub fn next(self: *Iterator) ?u32 {
+            const group = self.table.groups.get(self.index) orelse return null;
+            const codepoint = group.start_char_code + self.i;
+
+            if (codepoint < group.end_char_code) {
+                self.i += 1;
+                return codepoint;
+            } else {
+                self.i = 0;
+                self.index += 1;
+                return self.next(0);
+            }
+        }
+    };
+};
