f305cb870a8c9fe500e8a42c0b3dc24abadbf66f
Hollin Wilkins <hollinrwilkins@gmail.com>
2024-05-30 17:57:10 -0700

    Format10 implementation.

--- src/table/cmap/format10.zig
+++ src/table/cmap/format10.zig
@@ -1,45 +1,57 @@
-// use crate::parser::{LazyArray32, Stream};
-// use crate::GlyphId;
+const std = @import("std");
+const root = @import("../../root.zig");
+const Error = root.Error;
+const Reader = root.Reader;
+const GlyphId = root.GlyphId;
+const LazyIntArray = root.LazyIntArray;
 
-// /// A [format 10](https://docs.microsoft.com/en-us/typography/opentype/spec/cmap#format-10-trimmed-array)
-// /// subtable.
-// #[derive(Clone, Copy, Debug)]
-// pub struct Subtable10<'a> {
-//     /// First character code covered.
-//     pub first_code_point: u32,
-//     /// Array of glyph indices for the character codes covered.
-//     pub glyphs: LazyArray32<'a, GlyphId>,
-// }
+pub const Subtable10 = struct {
+    const GlyphsList = LazyIntArray(u32);
 
-// impl<'a> Subtable10<'a> {
-//     /// Parses a subtable from raw data.
-//     pub fn parse(data: &'a [u8]) -> Option<Self> {
-//         let mut s = Stream::new(data);
-//         s.skip::<u16>(); // format
-//         s.skip::<u16>(); // reserved
-//         s.skip::<u32>(); // length
-//         s.skip::<u32>(); // language
-//         let first_code_point = s.read::<u32>()?;
-//         let count = s.read::<u32>()?;
-//         let glyphs = s.read_array32::<GlyphId>(count)?;
-//         Some(Self {
-//             first_code_point,
-//             glyphs,
-//         })
-//     }
+    first_codepoint: u32,
+    glyphs: GlyphsList,
 
-//     /// Returns a glyph index for a code point.
-//     pub fn glyph_index(&self, code_point: u32) -> Option<GlyphId> {
-//         let idx = code_point.checked_sub(self.first_code_point)?;
-//         self.glyphs.get(idx)
-//     }
+    pub fn create(data: []const u8) Error!Subtable10 {
+        var reader = Reader.create(data);
+        reader.skip(u16); // format
+        reader.skip(u16); // reserved
+        reader.skip(u16); // length
+        reader.skip(u16); // language
 
-//     /// Calls `f` for each codepoint defined in this table.
-//     pub fn codepoints(&self, mut f: impl FnMut(u32)) {
-//         for i in 0..self.glyphs.len() {
-//             if let Some(code_point) = self.first_code_point.checked_add(i) {
-//                 f(code_point);
-//             }
-//         }
-//     }
-// }
+        const first_codepoint = reader.readInt(u32) orelse return error.InvalidTable;
+        const count = reader.readInt(u32) orelse return error.InvalidTable;
+        const glyphs = GlyphsList.read(reader, count) orelse return error.InvalidTable;
+
+        return Subtable10{
+            .first_codepoint = first_codepoint,
+            .glyphs = glyphs,
+        };
+    }
+
+    pub fn getGllyphIndex(self: Subtable10, codepoint32: u32) ?GlyphId {
+        const index = codepoint32 - self.first_codepoint;
+        return self.glyphs.get(index);
+    }
+
+    pub fn iterator(self: Subtable10) Iterator {
+        return Iterator{
+            .table = self,
+            .index = 0,
+        };
+    }
+
+    pub const Iterator = struct {
+        table: Subtable10,
+        index: u32,
+
+        pub fn next(self: *Iterator) ?u32 {
+            if (self.index >= self.table.glyphs.len) {
+                return null;
+            }
+
+            const codepoint = self.table.first_codepoint + self.index;
+            self.index += 1;
+            return codepoint;
+        }
+    };
+};
