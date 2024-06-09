55add90c8e44bcd5c6c8c7d23ce60ccc7e0d74c1
Hollin Wilkins <hollinrwilkins@gmail.com>
2024-06-08 17:17:31 -0700

    Add zig-gamedev dependency.

--- build.zig
+++ build.zig
@@ -90,6 +90,16 @@ pub fn build(b: *std.Build) void {
 
     const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
 
+    if (b.lazyDependency("zstbi", .{
+        .target = target,
+        .optimize = optimize,
+    })) |dep| {
+        draw_glyph_exe.root_module.addImport("zstbi", dep.module("root"));
+        draw_glyph_exe.linkLibrary(dep.artifact("zstbi"));
+        exe_unit_tests.root_module.addImport("zstbi", dep.module("root"));
+        exe_unit_tests.linkLibrary(dep.artifact("zstbi"));
+    }
+
     // Similar to creating the run step earlier, this exposes a `test` step to
     // the `zig build --help` menu, providing a way for the user to request
     // running the unit tests.
