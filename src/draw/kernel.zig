45bb1d0022adf8b791e7e881b5216ecbd5ba8d52
Hollin Wilkins <hollinrwilkins@gmail.com>
2024-06-29 12:13:27 -0700

    Define interface for fill flattens.

--- src/draw/kernel.zig
+++ src/draw/kernel.zig
@@ -1,16 +1,26 @@
+const core = @import("../core/root.zig");
 const soup_module = @import("./soup.zig");
-const path = @import("./path.zig");
-const PathRecord = soup_module.PathRecord;
-const SubpathRecord = soup_module.SubpathRecord;
-const CurveRecord = soup_module.CurveRecord;
+const shape_module = @import("./shape.zig");
+const TransformF32 = core.TransformF32;
+const FlatPath = soup_module.FlatPath;
+const FlatSubpath = soup_module.FlatSubpath;
+const FlatCurve = soup_module.FlatCurve;
+const Path = shape_module.Path;
+const Subpath = shape_module.Subpath;
+const Curve = shape_module.Curve;
 
 pub fn Kernel(comptime T: type) type {
     return struct {
         pub fn flattenFill(
-            path: []const PathRecord,
-            subpath: []const SubpathRecord,
-            curve: []const CurveRecord,
-            source_curve: []const path.CurveRecord,
+            // input buffers
+            transforms: []TransformF32.Matrix,
+            curves: []const Curve,
+            flat_curves: []const FlatCurve,
+            // job parameters
+            transform_index: u32,
+            curve_index: u32,
+            flat_curve_index: u32,
+            // write destination
             items: []T,
         ) void {}
     };
