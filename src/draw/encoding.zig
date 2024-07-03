efdc237aae2bfe3b1b56ded9eab49d7aff2b292a
Hollin Wilkins <hollinrwilkins@gmail.com>
2024-07-02 23:31:37 -0700

    Move tests into better file.

--- src/draw/encoding.zig
+++ src/draw/encoding.zig
@@ -755,3 +755,70 @@ pub const CpuRasterizer = struct {
         std.debug.print("======================================\n", .{});
     }
 };
+
+test "encoding path monoids" {
+    var encoder = Encoder.init(std.testing.allocator);
+    defer encoder.deinit();
+
+    try encoder.encodeColor(ColorU8{
+        .r = 255,
+        .g = 255,
+        .b = 0,
+        .a = 255,
+    });
+    var style = Style{};
+    style.setFill(Style.Fill{
+        .brush = .color,
+    });
+    try encoder.encodeStyle(style);
+
+    var path_encoder = encoder.pathEncoder(f32);
+    try path_encoder.moveTo(core.PointF32.create(1.0, 1.0));
+    _ = try path_encoder.lineTo(core.PointF32.create(2.0, 2.0));
+    _ = try path_encoder.arcTo(core.PointF32.create(3.0, 3.0), core.PointF32.create(4.0, 2.0));
+    _ = try path_encoder.lineTo(core.PointF32.create(1.0, 1.0));
+    try path_encoder.finish();
+
+    var path_encoder2 = encoder.pathEncoder(i16);
+    try path_encoder2.moveTo(core.PointI16.create(10, 10));
+    _ = try path_encoder2.lineTo(core.PointI16.create(20, 20));
+    _ = try path_encoder2.lineTo(core.PointI16.create(15, 30));
+    _ = try path_encoder2.quadTo(core.PointI16.create(33, 44), core.PointI16.create(100, 100));
+    _ = try path_encoder2.cubicTo(
+        core.PointI16.create(120, 120),
+        core.PointI16.create(70, 130),
+        core.PointI16.create(22, 22),
+    );
+    try path_encoder2.finish();
+
+    const encoding = encoder.encode();
+    var rasterizer = CpuRasterizer.init(std.testing.allocator, encoding);
+    defer rasterizer.deinit();
+
+    try rasterizer.rasterize();
+
+    // rasterizer.debugPrint();
+    const path_monoids = rasterizer.path_monoids.items;
+
+    try std.testing.expectEqualDeep(
+        core.LineF32.create(core.PointF32.create(1.0, 1.0), core.PointF32.create(2.0, 2.0)),
+        encoding.getSegment(core.LineF32, path_monoids[0]),
+    );
+    try std.testing.expectEqualDeep(
+        core.ArcF32.create(
+            core.PointF32.create(2.0, 2.0),
+            core.PointF32.create(3.0, 3.0),
+            core.PointF32.create(4.0, 2.0),
+        ),
+        encoding.getSegment(core.ArcF32, path_monoids[1]),
+    );
+    try std.testing.expectEqualDeep(
+        core.LineF32.create(core.PointF32.create(4.0, 2.0), core.PointF32.create(1.0, 1.0)),
+        encoding.getSegment(core.LineF32, path_monoids[2]),
+    );
+
+    try std.testing.expectEqualDeep(
+        core.LineI16.create(core.PointI16.create(10, 10), core.PointI16.create(20,20)),
+        encoding.getSegment(core.LineI16, path_monoids[3]),
+    );
+}
