7a17e6caf39570136e4e154fe7646b1237d206b3
Hollin Wilkins <hollinrwilkins@gmail.com>
2024-08-24 23:23:19 -0700

    This is getting a bit messy.

--- src/draw/cpu/root.zig
+++ src/draw/cpu/root.zig
@@ -6,6 +6,7 @@ const msaa_module = @import("../msaa.zig");
 const Allocator = std.mem.Allocator;
 const RangeU32 = core.RangeU32;
 const PointF32 = core.PointF32;
+const LineF32 = core.LineF32;
 const Encoding = encoding_module.Encoding;
 const PathTag = encoding_module.PathTag;
 const PathMonoid = encoding_module.PathMonoid;
@@ -39,7 +40,18 @@ pub const CpuRasterizer = struct {
                 u32,
                 config.buffer_sizes.offsetsSize() + 2,
             ),
+            .bumps = try allocator.alloc(
+                std.atomic.Value(u32),
+                config.buffer_sizes.bumpsSize(),
+            ),
+            .lines = try allocator.alloc(
+                LineF32,
+                config.buffer_sizes.linesSize(),
+            ),
         };
+        for (buffers.bumps) |*bump| {
+            bump.raw = 0;
+        }
 
         return @This(){
             .allocator = allocator,
@@ -82,9 +94,17 @@ pub const DebugFlags = struct {
 };
 
 pub const BufferSizes = struct {
+    pub const DEFAULT_LINES_SIZE: u32 = 60;
     pub const DEFAULT_PATH_MONOIDS_SIZE: u32 = 60;
+    pub const DEFAULT_PATHS_SIZE: u32 = 10;
 
+    paths_size: u32 = DEFAULT_PATHS_SIZE,
     path_monoids_size: u32 = DEFAULT_PATH_MONOIDS_SIZE,
+    lines_size: u32 = DEFAULT_LINES_SIZE,
+
+    pub fn bumpsSize(self: @This()) u32 {
+        return self.paths_size * 2;
+    }
 
     pub fn pathMonoidsSize(self: @This()) u32 {
         return self.path_monoids_size;
@@ -93,12 +113,18 @@ pub const BufferSizes = struct {
     pub fn offsetsSize(self: @This()) u32 {
         return self.pathMonoidsSize() * 2;
     }
+
+    pub fn linesSize(self: @This()) u32 {
+        return self.lines_size;
+    }
 };
 
 pub const Buffers = struct {
     sizes: BufferSizes,
     path_monoids: []PathMonoid,
     offsets: []u32,
+    bumps: []std.atomic.Value(u32),
+    lines: []LineF32,
 };
 
 pub const PathMonoidExpander = struct {
@@ -160,6 +186,7 @@ pub const PathMonoidExpander = struct {
         }
 
         return State{
+            .path_offset = self.path_index,
             .segment_range = RangeU32.create(
                 start_segment_offset,
                 end_segment_offset,
@@ -170,6 +197,7 @@ pub const PathMonoidExpander = struct {
     }
 
     pub const State = struct {
+        path_offset: u32,
         segment_range: RangeU32,
         path_tags: []const PathTag,
         path_monoids: []const PathMonoid,
@@ -232,3 +260,70 @@ pub const LineCalculator = struct {
         offsets: []const u32,
     };
 };
+
+// pub const Flattener = struct {
+//     kernel_config: KernelConfig,
+//     encoding: *const Encoding,
+//     buffers: *const Buffers,
+//     debug_flags: *const DebugFlags,
+//     path_offset: u32 = 0,
+//     path_tags: []const PathTag,
+//     path_monoids: []const PathMonoid,
+//     offsets: []const u32,
+//     path_index: u32 = 0,
+
+//     pub fn flatten(self: *@This()) ?State {
+//         const path_index = self.path_index + self.path_offset;
+//         if (path_index >= self.encoding.paths.len) {
+//             return null;
+//         }
+
+//         const segment_offset = if (self.path_index > 0) self.encoding.path_offsets[self.path_index - 1] else 0;
+//         const start_path_segment_offset = if (path_index > 0) self.encoding.path_offsets[path_index - 1] - segment_offset;
+//         const start_fill_offset = self.offsets[start_path_segment_offset];
+//         const start_stroke_offset = self.offsets[self.path_tags.len + start_path_segment_offset];
+//         var end_fill_offset = start_fill_offset;
+//         var end_stroke_offset = start_stroke_offset;
+//         var fill_lines: u32 = 0;
+//         var stroke_lines: u32 = 0;
+
+//         var next_path_index = path_index;
+//         while (true) {
+//             if (next_path_index > self.encoding.path_offsets.len) {
+//                 break;
+//             }
+
+//             const next_path_segment_offset = self.encoding.path_offsets[next_path_index] - segment_offset;
+//             const next_end_fill_offset = self.offsets[next_path_segment_offset];
+//             const next_end_stroke_offset = self.offsets[self.path_tags.len + next_path_segment_offset];
+//             const next_fill_lines = next_end_fill_offset - start_fill_offset;
+//             const next_stroke_lines = next_end_stroke_offset - start_stroke_offset;
+
+//             if (next_fill_lines + next_stroke_lines > self.buffers.lines.len) {
+//                 break;
+//             }
+
+//             end_fill_offset = next_end_fill_offset;
+//             end_stroke_offset = next_end_stroke_offset;
+//             fill_lines = next_fill_lines;
+//             stroke_lines = next_stroke_lines;
+
+//             next_path_index += 1;
+//         }
+
+//         const start_path_index = self.path_index;
+//         const end_path_index = start_path_index + (next_path_index - path_index);
+//         self.path_index += next_path_index - path_index;
+//         if (fill_lines == 0 and stroke_lines == 0) {
+//             return;
+//         }
+
+
+
+//         return null;
+//     }
+
+//     pub const State = struct {
+//         lines: []const LineF32,
+//     };
+// };
